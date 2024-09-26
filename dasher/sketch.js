570f30d5e54a74775a8ed700c4ba7cb3ea9de812
EVE <104692088+PinkPlague@users.noreply.github.com>
2024-09-25 19:30:00 -0600

    dasher updates

--- dasher/sketch.js
+++ dasher/sketch.js
@@ -7,8 +7,11 @@ let playerX;
 let playerY;
 let playerSpeed;
 let playerWeight;
+let jump = 0;
 
 let onGround;
+
+let detect;
 
 function setup() {
   windowDesired = 600;
@@ -19,18 +22,21 @@ function setup() {
   
   playerSize = 50;
   playerY = 300;
-  playerX = 0;
+  playerX = 50;
   playerSpeed = 10;
-  playerWeight = 5;
+  playerWeight = 1;
+  jump = 0;
 
-  onGround = true;
+  onGround = false;
+
+  detect = 255;
 }
 
 function draw() {
   background(220);
 
-
-
+  
+  playerY -= jump;
 
   up();
   // down();
@@ -41,38 +47,49 @@ function draw() {
 
   fill(0);
   square(playerX, playerY, playerSize);
+
+
+  fill(detect);
+  square(150, 500, 100)
+
+
+
+  if(playerX+playerSize >= 150 && playerX <= (150 + 100) && playerY+playerSize >= 500 && playerY <= (600 + 100)) {
+    detect = 0;
+  }
+  else {
+    detect = 255;
+  }
 }
 
 
 function gravity() {
-  if (playerY+playerSize>=winHeight) {
+  if (playerY+playerSize>=winHeight-100) {
     onGround = true;
-    playerY = winHeight - playerSize;
-  }
-  else {
+    playerY = winHeight - playerSize-100;
+    jump = 1;
+  } else {
     onGround = false;
   }
   if (!onGround) {
-    playerY += playerWeight;
-  }
-}
-function left() {
-  if (keyIsDown(LEFT_ARROW) === true) {
-    playerX -= playerSpeed;
-  }
-}
-function right() {
-  if (keyIsDown(RIGHT_ARROW) === true) {
-    playerX += playerSpeed;
+    // playerY += playerWeight;
+    jump -= playerWeight
   }
 }
 function up() {
   if (keyIsDown(UP_ARROW) === true) {
-    playerY -= playerSpeed;
+    if (onGround) {
+      jump += 15
+    }
   }
 }
-// function down() {
-//   if (keyIsDown(DOWN_ARROW) === true) {
-//     playerY += playerSpeed;
-//   }
-// }
+function right() {
+  if (keyIsDown(RIGHT_ARROW) === true) {
+    playerX += 2;
+  }
+}
+function left() {
+  if (keyIsDown(LEFT_ARROW) === true) {
+    playerX -= 2;
+  }
+}
