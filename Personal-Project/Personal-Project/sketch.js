a71a494fd2bcfb9ace253db256b55f6a804d6240
EVE <104692088+PinkPlague@users.noreply.github.com>
2024-10-21 13:55:31 -0600

    silly personal project update

--- Personal-Project/Personal-Project/sketch.js
+++ Personal-Project/Personal-Project/sketch.js
@@ -7,34 +7,38 @@ let score = 0;
 
 /* SETUP RUNS ONCE */
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  world.gravity.y = 10;
+  createCanvas(windowWidth-5, windowHeight-5);
+  // world.gravity.y = 10;
 
   //Create player
-  player = new Sprite(50, 250, 40, 40);
+  player = new Sprite(50, 250, 25, 25);
   player.color = 'grey';
   player.rotationLock = true;
   player.vel.x = 0;
   player.vel.y = 0;
+  player.bounciness = 0;
 
   //Create ground
-  ground = new Sprite(150, windowHeight - 150, windowWidth, 400, "s");
+  ground = new Sprite(150, windowHeight, windowWidth, 200, "s");
   ground.color = color(188, 158, 130);
   ground.friction = 0;
+  ground.bounciness = 0;
 
   //Create platforms group
   platforms = new Group();
   platforms.color = "blue";
   platforms.collider = "s";
   platforms.friction = 0;
+  platforms.bounciness = 0;
+  platforms.rotation = 45;
 
   //Create coins group
-  coins = new Group();
-  coins.color = "yellow";
-  coins.collider = "k";
+  // coins = new Group();
+  // coins.color = "yellow";
+  // coins.collider = "k";
 
   //Overlaps method takes in a Sprite or group name (coins), then calls a function (collect)
-  player.overlaps(coins, collect);
+  // player.overlaps(coins, collect);
 
   //Load starting screen
   loadStartScreen();
@@ -53,25 +57,30 @@ function draw() {
   text('Collect all 8 coins to win!', 10, 30);
 
   //Move the player
-  if (kb.presses("up")) {
+  if (kb.pressing("up")) {
     player.vel.y = -6;
   }
-
-  if (kb.pressing("left")) {
-    player.vel.x = -3;
-  } else if (kb.pressing("right")) {
-    player.vel.x = 3;
-  } else {
-    player.vel.x = 0;
+  else {
+    player.vel.y = 6;
   }
+
+
+  player.vel.x = 6;
+  // if (kb.pressing("left")) {
+  //   player.vel.x = -6;
+  // } else if (kb.pressing("right")) {
+  //   player.vel.x = 6;
+  // } else {
+  //   player.vel.x = 0;
+  // }
 
   //Stop player from moving outside of screen
   if (player.x < 20) {
     player.x = 20;
   }
   
-  if (player.x > 2000) {
-    player.x = 900;
+  if (player.x > 6000) {
+    player.x = 20;
   }
   
   if (player.y < 20){
@@ -91,24 +100,29 @@ function draw() {
 /* FUNCTIONS */
 function loadStartScreen() {
   platforms.removeAll();
-  coins.removeAll();
+  // coins.removeAll();
 
   //Move player to starting position
   player.x = 50;
 
   //Create two platforms
-  new platforms.Sprite(110, windowHeight - 400, 50, 100);
-  new platforms.Sprite(260, 200, 150, 30);
+  new platforms.Sprite(400, windowHeight - 100, 700, 700);
+  new platforms.Sprite(800, windowHeight - 100, 700, 700);
+  new platforms.Sprite(1200, windowHeight - 100, 700, 700);
+  new platforms.Sprite(1400, windowHeight - 100, 700, 700);
+  new platforms.Sprite(600, 0, 700, 700);
+  new platforms.Sprite(1000, 0, 700, 700);
+  new platforms.Sprite(1300, 0, 700, 700);
 
   //Create eight coins
-  new coins.Sprite(220, 170, 15);
-  new coins.Sprite(260, 170, 15);
-  new coins.Sprite(300, 170, 15);
-  new coins.Sprite(600, 350, 15);
-  new coins.Sprite(640, 350, 15);
-  new coins.Sprite(680, 350, 15);
-  new coins.Sprite(810, 350, 15);
-  new coins.Sprite(850, 350, 15);
+  // new coins.Sprite(220, 170, 15);
+  // new coins.Sprite(260, 170, 15);
+  // new coins.Sprite(300, 170, 15);
+  // new coins.Sprite(600, 350, 15);
+  // new coins.Sprite(640, 350, 15);
+  // new coins.Sprite(680, 350, 15);
+  // new coins.Sprite(810, 350, 15);
+  // new coins.Sprite(850, 350, 15);
 }
 
 function reset() {
