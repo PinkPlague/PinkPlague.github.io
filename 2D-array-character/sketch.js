51efef02a81ceeb8960eccc82ce73305fcbdfb3c
EVE <104692088+PinkPlague@users.noreply.github.com>
2024-10-30 14:15:46 -0600

    started minesweeper

--- 2D-array-character/sketch.js
+++ 2D-array-character/sketch.js
@@ -10,25 +10,34 @@
 
 let grid;
 
-const GRID_SIZE = 40;
+const GRID_SIZE = 10;
 let cellSize;
 
 let shouldToggleNeighbours = false;
 
-const OPEN_TILE = 0;
-const IMPASSIBLE_TILE = 1;
-const PLAYER_TILE = 2;
-const GRASS_TILE = 3;
-
-let player = {
-  x: 3,
-  y: 5,
+const BLANK_TILE = 0;
+const MINE_TILE = -1;
+const NUMBER_TILES = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight:8,
 };
 
 function preload() {
-  lavaimg = loadImage("assets/textures/lava.png");
-  pathimg = loadImage("assets/textures/path.png");
-  grassimg = loadImage("assets/textures/grass.jpg");
+  mineImg = loadImage("assets/textures/mine.png");
+  oneImg = loadImage("assets/textures/1.png");
+  twoImg = loadImage("assets/textures/2.png");
+  threeImg = loadImage("assets/textures/3.png");
+  fourImg = loadImage("assets/textures/4.png");
+  fiveImg = loadImage("assets/textures/5.png");
+  sixImg = loadImage("assets/textures/6.png");
+  sevenImg = loadImage("assets/textures/7.png");
+  eightImg = loadImage("assets/textures/8.png");
 }
 
 function setup() {
@@ -40,14 +49,13 @@ function setup() {
   }
   cellSize = height/GRID_SIZE;
   grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
-
-  // add player to grid
-  grid[player.y][player.x] = PLAYER_TILE;
+  updateGrid();
 }
 
 function draw() {
   background(220);
   displayGrid();
+  
 }
 
 function windowResized() {
@@ -76,11 +84,11 @@ function mousePressed() {
 
 function toggleCell(x,y) {
   if (x >= 0 && y >= 0 && x < GRID_SIZE && y < GRID_SIZE) {
-    if (grid[y][x] === IMPASSIBLE_TILE) {
-      grid[y][x] = OPEN_TILE;
+    if (grid[y][x] === BLANK_TILE) {
+      grid[y][x] = BLANK_TILE;
     }
-    else if (grid[y][x] === OPEN_TILE) {
-      grid[y][x] = IMPASSIBLE_TILE;
+    else if (grid[y][x] === BLANK_TILE) {
+      grid[y][x] = BLANK_TILE;
     }
   }
 }
@@ -89,70 +97,45 @@ function keyPressed() {
   if (key === "r") {
     grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
   }
-  if (key === "e") {
-    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  }
-  if (key === "n") {
-    shouldToggleNeighbours = !shouldToggleNeighbours;
-  }
-  if (key === "w") {
-    //move up
-    movePlayer(player.x, player.y - 1);
-  }
-  if (key === "s") {
-    //move down
-    movePlayer(player.x, player.y + 1);
-  }
-  if (key === "d") {
-    //move right
-    movePlayer(player.x + 1, player.y);
-  }
-  if (key === "a") {
-    //move right
-    movePlayer(player.x - 1, player.y);
-  }
-}
-
-function movePlayer(x, y) {
-  // keep in-bounds
-  if (x >= 0   && x < GRID_SIZE && y >= 0 && y < GRID_SIZE 
-    && grid[y][x] === OPEN_TILE
-  ) {
-    // reset last pos
-    if (grid[y][x] === GRASS_TILE) {
-      grid[player.y][player.x] = GRASS_TILE;
-    }
-    else {
-      grid[player.y][player.x] = OPEN_TILE;
-    }
-  
-    // last saved location
-    player.x = x;
-    player.y = y;
-  
-    // return player to grid
-    grid[player.y][player.x] = PLAYER_TILE;
-  }
-
+  // if (key === "n") {
+  //   shouldToggleNeighbours = !shouldToggleNeighbours;
+  // }
 }
 
 function displayGrid() {
   for (let y = 0; y < GRID_SIZE; y++) {
     for (let x = 0; x < GRID_SIZE; x++) {
-      if (grid[y][x] === IMPASSIBLE_TILE) {
+      if (grid[y][x] === BLANK_TILE) {
         // fill(0);
-        image(lavaimg, x * cellSize, y * cellSize, cellSize, cellSize);
-      }
-      else if (grid[y][x] === OPEN_TILE) {
-        // fill(255);
-        image(pathimg, x * cellSize, y * cellSize, cellSize, cellSize);
-      } 
-      else if (grid[y][x] === GRASS_TILE) {
-        image(grassimg, x * cellSize, y * cellSize, cellSize, cellSize0);
-      }
-      else if (grid[y][x] === PLAYER_TILE) {
-        fill(255,0,0);
         square(x * cellSize, y * cellSize, cellSize);
+      }
+      else if (grid[y][x] === MINE_TILE) {
+        // fill(255);
+        image(mineImg, x * cellSize, y * cellSize, cellSize, cellSize);
+      } 
+      else if (grid[y][x] === NUMBER_TILES.one) {
+        image(oneImg, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      else if (grid[y][x] === NUMBER_TILES.two) {
+        image(twoImg, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      else if (grid[y][x] === NUMBER_TILES.three) {
+        image(threeImg, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      else if (grid[y][x] === NUMBER_TILES.four) {
+        image(fourImg, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      else if (grid[y][x] === NUMBER_TILES.five) {
+        image(fiveImg, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      else if (grid[y][x] === NUMBER_TILES.six) {
+        image(sixImg, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      else if (grid[y][x] === NUMBER_TILES.seven) {
+        image(sevenImg, x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      else if (grid[y][x] === NUMBER_TILES.eight) {
+        image(eightImg, x * cellSize, y * cellSize, cellSize, cellSize);
       }
     }
   }
@@ -165,14 +148,15 @@ function generateRandomGrid(cols, rows) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
       //choose either 0 or 1, each 50% of the time
-      if (random(100) < 50) {
-        newGrid[y].push(IMPASSIBLE_TILE);
+      if (random(100) < 20) {
+        newGrid[y].push(MINE_TILE);
       }
       else {
-        newGrid[y].push(OPEN_TILE);
+        newGrid[y].push(BLANK_TILE);
       }
     }
   }
+
   return newGrid;
 }
 
@@ -183,8 +167,63 @@ function generateEmptyGrid(cols, rows) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
       //choose either 0 or 1, each 50% of the time
-      newGrid[y].push(OPEN_TILE);
+      newGrid[y].push(BLANK_TILE);
     }
   }
   return newGrid;
 }
+
+function updateGrid() {
+  //make a new array to hold the next turn
+
+  //look at every cell
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      //count it's neighbours
+      let neighbours = 0;
+
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          //don't fall of the edge
+          if (y+i >= 0 && y+i < GRID_SIZE && x+j >= 0 && x+j < GRID_SIZE) {
+            if (grid[y+i][x+j] === -1) {
+              neighbours += grid[y+i][x+j];
+            }
+          }
+        }
+      }
+
+      //don't count yourself
+      neighbours -= grid[y][x];
+
+      if (neighbours === -NUMBER_TILES.one) {
+        grid[y][x] = NUMBER_TILES.one;
+      }
+      else if (neighbours === -NUMBER_TILES.two) {
+        grid[y][x] = NUMBER_TILES.two;
+      }
+      else if (neighbours === -NUMBER_TILES.three) {
+        grid[y][x] = NUMBER_TILES.three;
+      }
+      else if (neighbours === -NUMBER_TILES.four) {
+        grid[y][x] = NUMBER_TILES.four;
+      }
+      else if (neighbours === -NUMBER_TILES.five) {
+        grid[y][x] = NUMBER_TILES.five;
+      }
+      else if (neighbours === -NUMBER_TILES.six) {
+        grid[y][x] = NUMBER_TILES.six;
+      }
+      else if (neighbours === -NUMBER_TILES.seven) {
+        grid[y][x] = NUMBER_TILES.seven;
+      }
+      else if (neighbours === -NUMBER_TILES.eight) {
+        grid[y][x] = NUMBER_TILES.eight;
+      }
+
+
+      
+    }
+  }
+}
+
